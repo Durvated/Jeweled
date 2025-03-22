e6ed6c9d45b2e687c03c4fecd152989684e25fd0
Vivian <durvated@gmail.com>
2025-03-22 08:02:10 -0500

    a:b
    Signed-off-by: Vivian <durvated@gmail.com>

--- JS/blank.js
+++ JS/blank.js
@@ -1,34 +1,33 @@
-<script>
-    document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     history.replaceState({ page: "home" }, "", window.location.href);
 
     const openBlankLink = document.getElementById('openBlankLink');
 
     openBlankLink.addEventListener('click', (event) => {
-    event.preventDefault();
+        event.preventDefault();
 
-    const newTab = window.open('about:blank', '_blank');
-    if (!newTab) {
-    alert("Couldn't manage to open a new tab :(");
-    return;
-}
+        const newTab = window.open('about:blank', '_blank');
+        if (!newTab) {
+            alert("Couldn't manage to open a new tab :(");
+            return;
+        }
 
-    const newTabBody = newTab.document.body;
-    Object.assign(newTabBody.style, { padding: '0', margin: '0', border: 'hidden' });
+        const newTabBody = newTab.document.body;
+        Object.assign(newTabBody.style, { padding: '0', margin: '0', border: 'hidden' });
 
-    const iframe = document.createElement('iframe');
-    Object.assign(iframe.style, { width: '100%', height: '100%', border: 'hidden' });
-    iframe.src = window.location.href;
+        const iframe = document.createElement('iframe');
+        Object.assign(iframe.style, { width: '100%', height: '100%', border: 'hidden' });
+        iframe.src = window.location.href;
 
-    iframe.onload = () => {
-    const links = iframe.contentDocument.querySelectorAll('a[target="_blank"]');
-    links.forEach(link => {
-    link.addEventListener('click', (event) => {
-    event.preventDefault();
-    iframe.contentWindow.location.href = link.href;
-});
-});
-};
+        iframe.onload = () => {
+            const links = iframe.contentDocument.querySelectorAll('a[target="_blank"]');
+            links.forEach(link => {
+                link.addEventListener('click', (event) => {
+                    event.preventDefault();
+                    iframe.contentWindow.location.href = link.href;
+                });
+            });
+        };
 
-    newTabBody.appendChild(iframe);
-});
+        newTabBody.appendChild(iframe);
+    })}
