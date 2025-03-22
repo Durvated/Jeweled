e6c15f109ea067d097bff953fedb7c4e322efe9a
Vivian <durvated@gmail.com>
2025-03-22 05:41:23 -0500

    fix ac
    Signed-off-by: Vivian <durvated@gmail.com>

--- GemClicker/amethystClicker.js
+++ GemClicker/amethystClicker.js
@@ -1,9 +1,17 @@
 let amethyst = document.getElementById('amethyst');
 let shards = 0;
 let cps = 0;
+let minerButton= document.getElementById('minerButton');
 let miner = 15;
 let minerCPS = 1;
 let minerCount = 0;
+minerButton.addEventListener('click', () => {
+    if(shards>= miner){
+        shards-=miner;
+        minerCount+=1
+        miner*=1.05;
+    }
+})
 amethyst.addEventListener('click', () => {
     shards+=1;
     shardCount.textContent = shards;
@@ -12,7 +20,8 @@ amethyst.addEventListener('click', () => {
 function spsSetter(){
 for (let i = 0; i <= minerCount; i++){}
 cps+=minerCPS
+    return cps
 }
 function updateScreen(){
-    shards+=spsSetter;
+    shards+=cps;
 }
