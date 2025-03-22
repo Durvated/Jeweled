80ff3c196572099d728921d8b2e24697612b6eaf
Vivian <durvated@gmail.com>
2025-03-22 02:57:11 -0500

    page creation, css, index, js

--- JS/more.js
+++ JS/more.js
@@ -1,10 +1,3 @@
-document.addEventListener("mousemove", (e) => {
-    let intensity = 20;
-    let x = (e.clientX / window.innerWidth - 0.5) * intensity; // Adjust intensity
-    let y = (e.clientY / window.innerHeight - 0.5) * intensity;
-
-    document.body.style.backgroundPosition = `${50 + x}% ${50 + y}%`;
-});
 
 class MovingButton {
     constructor(element) {
