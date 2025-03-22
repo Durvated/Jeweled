e01cf41445a0c6dd3dabd3d33bd33db2aa7e4851
Vivian <durvated@gmail.com>
2025-03-22 07:59:49 -0500

    a:b
    Signed-off-by: Vivian <durvated@gmail.com>

--- JS/blank.js
+++ JS/blank.js
@@ -1,7 +1,34 @@
-document.getElementById('changeUrlButton').addEventListener('click', function() {
-    var originalContent = document.body.innerHTML;
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+    history.replaceState({ page: "home" }, "", window.location.href);
 
-    history.pushState(null, '', 'about:blank');
+    const openBlankLink = document.getElementById('openBlankLink');
 
-    document.body.innerHTML = originalContent;
+    openBlankLink.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const newTab = window.open('about:blank', '_blank');
+    if (!newTab) {
+    alert("Couldn't manage to open a new tab :(");
+    return;
+}
+
+    const newTabBody = newTab.document.body;
+    Object.assign(newTabBody.style, { padding: '0', margin: '0', border: 'hidden' });
+
+    const iframe = document.createElement('iframe');
+    Object.assign(iframe.style, { width: '100%', height: '100%', border: 'hidden' });
+    iframe.src = window.location.href;
+
+    iframe.onload = () => {
+    const links = iframe.contentDocument.querySelectorAll('a[target="_blank"]');
+    links.forEach(link => {
+    link.addEventListener('click', (event) => {
+    event.preventDefault();
+    iframe.contentWindow.location.href = link.href;
+});
+});
+};
+
+    newTabBody.appendChild(iframe);
 });
