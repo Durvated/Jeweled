1f3b995142d9d6d9e90bdebe2a1b40ee6a3d7124
Vivian <durvated@gmail.com>
2025-06-04 17:20:53 -0500

    Gem Clicker Upgrade Test.
    Signed-off-by: Vivian <durvated@gmail.com>

--- GemClicker/amethystClicker.js
+++ GemClicker/amethystClicker.js
@@ -2,6 +2,8 @@ const amethyst = document.getElementById('amethyst');
 const shardCount = document.getElementById('shardCount');
 const cpsDisplay = document.getElementById('cpsDisplay');let shards = 0;
 const minerButton = document.getElementById('minerButton');
+const upgradeMinerButton = document.getElementById('upgradeMinerButton');
+const minerUpgradeCost = 30;
 let cps = 0;
 let miner = 15;
 let minerCPS = 1;
@@ -11,6 +13,15 @@ let minerCount = 0;
 amethyst.addEventListener('click', () => {
     shards += 1;
     updateScreen();
+});
+
+upgradeMinerButton.addEventListener('click', () => {
+    if(shards >= minerUpgradeCost) {
+        shards -= minerUpgradeCost;
+        minerCPS += 1;
+        upgradeMinerButton.style.display = 'none';
+        updateScreen();
+    }
 });
 
 minerButton.addEventListener('click', () => {
